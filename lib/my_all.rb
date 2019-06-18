1a35f2895e09c4e0fe267aefa4b4923df10ec75c
magical-flow-3186 <rregalado17@gmail.com>
2019-06-18 02:26:42 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,7 +4,7 @@ def my_all?(collection)
   i = 0 
   block_return_values = []
   while i < collection.length 
-  block_return_values << yeild
+  block_return_values << yeild(collection[i])
   i += 1 
 end
 end
